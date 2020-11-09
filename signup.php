f99bd378711cffb1843b162eea070dad3a9249c4
Lucy Huang <lha54@sfu.ca>
2020-11-09 15:05:32 -0800

    Nav Bar links to home page now

--- signup.php
+++ signup.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="CSS/content_page.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
     </head>
     <body>
@@ -12,7 +14,7 @@
     <!-- NAVIGATION BAR -->
     <div class="topnav">
         <div class="topnav-right">
-        <a href="#home">Home</a>
+        <a href="index.php">Home</a>
         <a href="signin.php">Sign In</a>
         <a href="signup.php">Sign Up</a>
         </div>
