c248d11fdabe7fa0b3f4f5297b138bc659a1542d
Justin Lau <justin_lau_5@sfu.ca>
2020-11-09 12:09:26 -0800

    Update housing_db.sql

--- housing_db.sql
+++ housing_db.sql
@@ -14,10 +14,6 @@ CREATE TABLE IF NOT EXISTS `Property` (
   PRIMARY KEY (`listing_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `Household`
---
-
 INSERT INTO `Property` (`listing_id`, `realtor`, `city`, `address`, `price`, `beds`, `baths`, `sqft`, `type`) VALUES
 ('R2515047', 'BRUNO BEZJAK', 'Surrey', '84 - 16336 23A AVE', 832000, 4, 4, 1808, "Townhouse"),
 ('R2515566', 'GILCO REAL ESTATE SERVICES', 'Surrey', '409 - 13789 107A AVE', 464853, 2, 2, 876, "Condo"),
@@ -28,3 +24,19 @@ INSERT INTO `Property` (`listing_id`, `realtor`, `city`, `address`, `price`, `be
 ('R2515103', 'SUTTON GROUP-WEST COAST REALTY', 'Surrey', '16230 90 AVE', 2688000, 6, 10, 8503, "House"),
 ('R2511070', 'ROYAL LEPAGE WEST REAL ESTATE SERVICES', 'Surrey', '31 - 8266 KING GEORGE BLVD', 68800, 2, 1, 744, "Manufactured House"),
 ('R2514986', 'REGENT PARK REALTY INC.', 'Surrey', '307 - 10581 140 ST', 620000, 2, 2, 1152, "Condo");
+
+
+
+DROP TABLE IF EXISTS `Members`;
+CREATE TABLE IF NOT EXISTS `Members` (
+  `email` varchar(100) DEFAULT NULL,
+  `realtor` varchar(100) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `beds` float DEFAULT NULL,
+  `baths` float DEFAULT NULL,
+  `sqft` float DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
