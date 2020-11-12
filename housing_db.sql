3a40719fbacffc01572e1bdd5f0987c5c50a8f4d
Justin Lau <justin_lau_5@sfu.ca>
2020-11-11 20:31:50 -0800

    moved agent table before property table to fix foreign key errors
    was giving an error because i tried to make a foreign key reference a table that wasn't created yet

--- housing_db.sql
+++ housing_db.sql
@@ -1,27 +1,60 @@
 -- CREATE DATABASE JUSTIN_LAU;
 
+USE JUSTIN_LAU;
+
+/*Table structure for table `agents` */
+DROP TABLE IF EXISTS `agents`;
+CREATE TABLE IF NOT EXISTS `agents` (
+  `agent_ID` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `ph_no` varchar(100) NOT NULL,
+  PRIMARY KEY (`agent_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `agents` WRITE;
+
+/*Data for the table `agents` */
+INSERT INTO `agents` (`agent_ID`, `email`, `name`, `ph_no`) VALUES
+(3738, 'bruno_bezjak@remax.ca', 'BRUNO BEZJAK', '(604) 567-2191'),
+(5641, 'gilco@remax.ca', 'GILCO REAL ESTATE SERVICES', '(604) 468-5361'),
+(7927, 'faith_wilson@remax.ca', 'FAITH WILSON GROUP', '(604) 328-5580'),
+(3679, 'sutton_group@remax.ca', 'SUTTON GROUP - 1ST WEST REALTY', '(604) 522-7054'),
+(6859, 'century21@remax.ca', 'CENTURY 21 COASTAL REALTY LTD', '(604) 998-9008'),
+(3041, 'harvy_jawanda@remax.ca', 'HARVY JAWANDA', '(604) 460-4254'),
+(8749, 'sutton_group@remax.ca', 'SUTTON GROUP-WEST COAST REALTY', '(604) 522-7054'),
+(5216, 'royal_lepage@remax.ca', 'ROYAL LEPAGE WEST REAL ESTATE SERVICES', '(604) 305-5148'),
+(4725, 'regent_park@remax.ca', 'REGENT PARK REALTY INC.', '(604) 801-6338');
+
+UNLOCK TABLES;
+
 /*Table structure for table `property` */
 DROP TABLE IF EXISTS `property`;
 CREATE TABLE `property` (
-  `listing_id` varchar(100) DEFAULT NULL,
-  `beds` float DEFAULT NULL,
-  `baths` float DEFAULT NULL,
-  `sqft` float DEFAULT NULL,
-  `date_listed` varchar(100) DEFAULT NULL,
-  `city` varchar(100) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
-  `price` float DEFAULT NULL,
-  `type` varchar(100) DEFAULT NULL,
-  `agent_ID` varchar(100) DEFAULT NULL,
+  `listing_id` varchar(100) NOT NULL,
+  `beds` int (11) NOT NULL,
+  `baths` int (11) NOT NULL,
+  `sqft` int (11) NOT NULL,
+  `date_listed` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `price` double NOT NULL,
+  `type` varchar(100) NOT NULL,
+  `property_agent_ID` int(11) NOT NULL,
   PRIMARY KEY (`listing_id`),
-  KEY `agent_ID` (`agentID`),
-  CONSTRAINT `Property_ibfk_1` FOREIGN KEY (`agent_ID`) REFERENCES `agents` (`agent_ID`)
+  -- KEY `agent_ID` (`agent_ID`),
+  -- CONSTRAINT `FK_PropertyAgent` FOREIGN KEY (`agent_ID`) REFERENCES `agents` (`agent_ID`)
+  -- CONSTRAINT `FK_PropertyAgent` FOREIGN KEY (`agent_ID`) REFERENCES `agents` (`agent_ID`)
+
+  KEY `property_agent_ID` (`property_agent_ID`),
+  CONSTRAINT `property_ibfk_1` FOREIGN KEY (`property_agent_ID`) REFERENCES `agents` (`agent_ID`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 LOCK TABLES `property` WRITE;
 
-/*Data for the table `property` */
-INSERT INTO `property` (`listing_id`, `beds`, `baths`, `sqft`, `date_listed`, `city`, `address`, `price`, `type`, `agent_ID`) VALUES
+-- /*Data for the table `property` */
+INSERT INTO `property` (`listing_id`, `beds`, `baths`, `sqft`, `date_listed`, `city`, `address`, `price`, `type`, `property_agent_ID`) VALUES
 ('R2515047', 4, 4, 1808, 'Nov 03, 2020', 'Surrey', '84 - 16336 23A AVE', 832000, 'Townhouse', 3738),
 ('R2515566', 2, 2, 876, 'Nov 05 2020', 'Surrey', '409 - 13789 107A AVE', 464853,  'Condo', 5641),
 ('R2515580', 4, 4, 1048, 'Nov 05 2020', 'Surrey', '419 - 9979 140 ST', 459000, 'Condo', 7927),
@@ -34,38 +67,18 @@ INSERT INTO `property` (`listing_id`, `beds`, `baths`, `sqft`, `date_listed`, `c
 
 UNLOCK TABLES;
 
-/*Table structure for table `agents` */
-DROP TABLE IF EXISTS `agents`;
-CREATE TABLE IF NOT EXISTS `agents` (
-  `agent_ID` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `ph_no` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`agent_ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-LOCK TABLES `offices` WRITE;
-
-/*Data for the table `agents` */
-INSERT INTO `agents` (`agent_ID`, `email`, `name`, `ph_no`) VALUES
-('3738', 'bruno_bezjak@remax.ca', 'BRUNO BEZJAK', '(604) 567-2191'),
-('5641', 'gilco@remax.ca', 'GILCO REAL ESTATE SERVICES', '(604) 468-5361'),
-('7927', 'faith_wilson@remax.ca', 'FAITH WILSON GROUP', '(604) 328-5580'),
-('3679', 'sutton_group@remax.ca', 'SUTTON GROUP - 1ST WEST REALTY', '(604) 522-7054'),
-('6859', 'century21@remax.ca', 'CENTURY 21 COASTAL REALTY LTD', '(604) 998-9008'),
-('3041', 'harvy_jawanda@remax.ca', 'HARVY JAWANDA', '(604) 460-4254'),
-('8749', 'sutton_group@remax.ca', 'SUTTON GROUP-WEST COAST REALTY', '(604) 522-7054'),
-('5216', 'royal_lepage@remax.ca', 'ROYAL LEPAGE WEST REAL ESTATE SERVICES', '(604) 305-5148'),
-('4725', 'regent_park@remax.ca', 'REGENT PARK REALTY INC.', '(604) 801-6338');
-
-UNLOCK TABLES;
-
 /*Table structure for table `members` */
 DROP TABLE IF EXISTS `members`;
 CREATE TABLE IF NOT EXISTS `members` (
-  `email` varchar(100) DEFAULT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `ph_no` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `ph_no` varchar(100) NOT NULL,
   PRIMARY KEY (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+
+
