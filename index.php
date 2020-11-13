4ab9cd2147c7f2d0a4ab2f3c51a60ed67448fa39
Eric <eric.j.lee8@gmail.com>
2020-11-12 20:12:27 -0800

    V2.1
    PHP for check box added

--- index.php
+++ index.php
@@ -11,6 +11,44 @@
     </head>
     <body>
 
+    <?php
+            // Credentials
+            define("DB_SERVER", "localhost");
+            define("DB_USER", "root");
+            define("DB_PASS", "");
+            define("DB_NAME", "JUSTIN_LAU");
+
+            // Creating database connection
+            function db_connect() {
+                $connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
+                return $connection;
+
+                // Test is connection succeeded
+                if(mysqli_connect_erro()) {
+                    // if connection failed, skip the reest of PHP code and print error
+                    die("Database connection failed: ". mysqli_connect_error() . " (" . mysqli_connect_errorno() . ")" );
+                }
+            }
+
+            // Disconnecting Database connection
+            function db_disconnect($connection) {
+                if(!isset($connection)) {
+                    mysqli_close($connection);
+                }
+            }
+
+            // Query Functions:
+
+            // Get all 
+            function find_all_db() {
+                global $db;
+                
+                $sql = "SELECT * FROM orders";
+                $result = mysqli_query($db, $sql);
+                return $result;
+            }
+        ?>
+
     <!-- NAVIGATION BAR -->
     <div class="topnav">
         <div class="topnav-right">
@@ -103,10 +141,7 @@
 
                                         <h6>Price Range:</h6> 
                                         <div class="checkbox">
-                                            <label><input type="checkbox" id="price_100" name="price_100" value="1"> Under $100k</label>
-                                        </div>
-                                        <div class="checkbox">
-                                            <label><input type="checkbox" id="price_500" name="price_500" value="1"> $100k - $500k</label>
+                                            <label><input type="checkbox" id="price_500" name="price_500" value="1"> Under $500k</label>
                                         </div>
                                         <div class="checkbox">
                                             <label><input type="checkbox" id="price_1m" name="price_1m" value="1"> $500k - $1mill</label>
@@ -114,7 +149,7 @@
                                         <div class="checkbox">
                                             <label><input type="checkbox" id="price_1over" name="price_1over" value="1"> Over $1mill</label>
                                         </div>
-
+<!--
                                         <h6>City:</h6> 
                                         <div class="checkbox">
                                             <label><input type="checkbox" id="city_vancouver" name="city_vancouver" value="1"> Vancouver</label>
@@ -131,7 +166,7 @@
                                         <div class="checkbox">
                                             <label><input type="checkbox" id="city_coquitlam" name="city_coquitlam" value="1"> Coquitlam</label>
                                         </div>
-
+-->
                                         <br>
                                         <div class="box">
                                             <div class="container">
@@ -148,6 +183,9 @@
                 </div>
 
                 <?php
+
+                $db = db_connect();
+
                     // PHP code for when the form is submitted as POST
                     if($_SERVER["REQUEST_METHOD"] == "POST") {
 
@@ -156,80 +194,79 @@
                         $type_condo = $_POST['type_condo'] ?? '0';
                         $type_townhouse = $_POST['type_townhouse'] ?? '0';
 
-                        $price_100 = $_POST['price_100'] ?? '0';
                         $price_500 = $_POST['price_500'] ?? '0';
                         $price_1m = $_POST['price_1m'] ?? '0';
                         $price_1over = $_POST['price_1over'] ?? '0';
 
+                        /*
                         $city_vancouver = $_POST['city_vancouver'] ?? '0';
                         $city_burnaby = $_POST['city_burnaby'] ?? '0';
                         $city_richmond = $_POST['city_richmond'] ?? '0';
                         $city_surrey = $_POST['city_surrey'] ?? '0';
-                        $city_coquitlam = $_POST['city_coquitlam'] ?? '0';
+                        $city_coquitlam = $_POST['city_coquitlam'] ?? '0'; 
+                        */
                     }
 
 
                     // Building Query request
-                    $send_sql = "SELECT * FROM ";
+                    $send_sql = "SELECT * FROM PROPERTY";
 
-                    
+
                     // Including checkbox field
-                    if($order_number == 1) {
-                        $send_sql .= "";
-                        if($type_condo == 1 || $type_townhouse == 1 || $price_100 == 1 || $price_500 == 1 || $price_1m == 1 || $price_1over == 1 || $city_vancouver == 1 || $city_burnaby == 1 || $city_richmond == 1 || $city_surrey == 1 || $city_coquitlam == 1) {
-                            $send_sql .= ",";
+                    if($type_house == 1 || type_condo == 1 || type_townhouse == 1) {
+                        $send_sql .= " WHERE (TYPE=";
+                        if($type_house == 1) {
+                            $send_sql .= "'House'";
+                            if(type_condo == 1 || type_townhouse == 1) {
+                                $send_sql .= " OR ";
+                            }
                         }
-                    }
-                    if($order_date == 1) {
-                        $send_sql .= " orders.orderDate";
-                        if($shipped_date == 1 || $product_name == 1 || $product_description == 1 || $quantity_ordered == 1 || $price_each == 1) {
-                            $send_sql .= ",";
+                        if(type_condo == 1) {
+                            $send_sql .= " 'Condo' ";
+                            if(type_townhouse == 1) {
+                                $send_sql .= " OR ";
+                            }
                         }
-                    }
-                    if($shipped_date == 1) {
-                        $send_sql .= " orders.shippedDate";
-                        if($product_name == 1 || $product_description == 1 || $quantity_ordered == 1 || $price_each == 1) {
-                            $send_sql .= ",";
+                        if(type_townhouse == 1) {
+                            $send_sql .= " 'Townhouse' ";
                         }
-                    }
-                    if($product_name == 1) {
-                        $send_sql .= " products.productName";
-                        if($product_description == 1 || $quantity_ordered == 1 || $price_each == 1) {
-                            $send_sql .= ",";
-                        }
-                    }
-                    if($product_description == 1) {
-                        $send_sql .= " products.productDescription";
-                        if($quantity_ordered == 1 || $price_each == 1) {
-                            $send_sql .= ",";
-                        }
-                    }
-                    if($quantity_ordered == 1) {
-                        $send_sql .= " orderdetails.quantityOrdered";
-                        if($price_each == 1) {
-                            $send_sql .= ",";
-                        }
-                    }
-                    if($price_each == 1) {
-                        $send_sql .= " orderdetails.priceEach";
-                    }
-                    
-                    $send_sql .= " FROM orders";
-
-                    // Connecting to Primary key of orderDetails table
-                    if($quantity_ordered == 1 || $price_each == 1) {
-                        $send_sql .= " INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber";
+                        $send_sql .= ")";
                     }
 
-                    // Connecting to Primary key of products table
-                    if($product_name == 1 || $product_description == 1)  {
-                        if($quantity_ordered != 1 || $price_each != 1) {
-                            $send_sql .= " INNER JOIN orderdetails ON orders.orderNumber = orderdetails.orderNumber";
+                    if($price_500 == 1 || $price_1m == 1 || $price_1over == 1) {
+
+                        if($type_house == 1 || type_condo == 1 || type_townhouse == 1) {
+                            $send_sql .= " AND (PRICE";
+                        } else {
+                            $send_sql .= " WHERE (PRICE";
                         }
-                        $send_sql .= " INNER JOIN products ON orderdetails.productCode = products.productCode";
+
+                        if($price_500 == 1 || $price_1m == 0 || $price_1over == 0) {
+                            $send_sql .= " < 500000)";
+                        }
+
+                        if($price_500 == 0 || $price_1m == 1 || $price_1over == 0) {
+                            $send_sql .= " >= 500000 && PRICE <= 1000000)";
+                        }
+
+                        if($price_500 == 0 || $price_1m == 0 || $price_1over == 1) {
+                            $send_sql .= " > 1000000)";
+                        }
+
+                        if($price_500 == 1 || $price_1m == 1 || $price_1over == 0) {
+                            $send_sql .= " < 1000000)";
+                        }
+                        if($price_500 == 0 || $price_1m == 1 || $price_1over == 1) {
+                            $send_sql .= " > 500000)";
+                        }
+                        if($price_500 == 1 || $price_1m == 0 || $price_1over == 1) {
+                            $send_sql .= " < 500000 || > 1000000)";
+                        }
                     }
 
                     $resultTable = mysqli_query($db, $send_sql);
+
+                    echo $send_sql;
                     
                 ?>
                 
@@ -237,21 +274,21 @@
                 <div class="col-6">
 
                 <?php   
-                    while($subject = mysqli_fetch_row($resultTable)) {
-                        echo "<div class=\"card flex-md-row mb-4 box-shadow h-md-250\">"
-                        echo "<img class=\"card-img-left flex-auto d-none d-md-block\" src=\"Images/house_1.jpg\" alt=\"Card image cap\" width=\"40%\">"
-                        echo "<h3 class=\"mb-0\">
-                                <a class=\"text-dark\" href=\"#\">$1,799,000</a>
-                            </h3>"
-                        echo "<strong class=\"d-inline-block mb-2 text-primary\">7 HATTFIELD PL|HAMILTON, ON, L9H 4J7</strong>
-                                <div class=\"mb-1 text-muted\">4 BED | 3 FULL BATH | 1 HALF BATH</div>
-                                <p class=\"card-text mb-auto\">2072 SQFT | HOUSE</p>
-                                <a href=\"content_page.php\">View Listing</a></div></div><br>"
+                    // while($subject = mysqli_fetch_row($resultTable)) {
+                    //     echo "<div class=\"card flex-md-row mb-4 box-shadow h-md-250\">"
+                    //     echo "<img class=\"card-img-left flex-auto d-none d-md-block\" src=\"Images/house_1.jpg\" alt=\"Card image cap\" width=\"40%\">"
+                    //     echo "<h3 class=\"mb-0\">
+                    //             <a class=\"text-dark\" href=\"#\">$1,799,000</a>
+                    //         </h3>"
+                    //     echo "<strong class=\"d-inline-block mb-2 text-primary\">7 HATTFIELD PL|HAMILTON, ON, L9H 4J7</strong>
+                    //             <div class=\"mb-1 text-muted\">4 BED | 3 FULL BATH | 1 HALF BATH</div>
+                    //             <p class=\"card-text mb-auto\">2072 SQFT | HOUSE</p>
+                    //             <a href=\"content_page.php\">View Listing</a></div></div><br>"
 
-                        foreach($subject as $value) {
-                            echo "<td>" . $value . "</td>";
-                        }
-                    }
+                    //     foreach($subject as $value) {
+                    //         echo "<td>" . $value . "</td>";
+                    //     }
+                    // }
 
                 ?>
 
