1e420dc109b1af9e22e9c5b8f1b38fb0033387fa
Justin Lau <justin_lau_5@sfu.ca>
2020-11-12 22:10:01 -0800

    14:28 in the video

--- server.php
+++ server.php
@@ -1,13 +1,22 @@
 <?php
 
-	//connect to the database
+//initialize variables
+$first_name = "";
+$last_name = "";
+$email = "";
+$password = "";
+$password_confirm = "";
+$errors = array();
+
+
+//connect to the database
 $dbhost = "localhost";
 $dbuser = "root";
 $dbpass = "";
 $dbname = "justin_lau";
-$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+$db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-	// Test if connection succeeded
+// Test if connection succeeded
 if(mysqli_connect_errno()) {
 	    // if connection failed, skip the rest of PHP code, and print an error
 	die("Database connection failed: " . 
@@ -19,75 +28,44 @@ if(mysqli_connect_errno()) {
 // if the register button is clicked
 if (isset($_POST['register'])) {
 
-	$first_name = 
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
-	$pw = $_POST['pw'];
-	$pwconfirm = $_POST['pwconfirm'];
-	$email = $_POST['email'];
+	$first_name = mysql_real_escape_string($_POST['first_name']);
+	$last_name = mysql_real_escape_string($_POST['last_name']);
+	$email = mysql_real_escape_string($_POST['email']);
+	$password = mysql_real_escape_string($_POST['password']);
+	$password_confirm = mysql_real_escape_string($_POST['password_confirm']);
+	
+	//ensure that form fields are filled properly
+	if (empty($first_name)) {
+		array_push($errors, "First name is required");
+	}
+	if (empty($last_name)) {
+		array_push($errors, "Last name is required");
+	}
+	if (empty($email)) {
+		array_push($errors, "Email is required");
+	}
+	if (empty($password)) {
+		array_push($errors, "Password is required");
+	}
 
-
-
-
-	if($pw != $pwconfirm) {
+	if ($password != $password_confirm) {
 		array_push($errors, "The two passwords do not match");
-		echo "The two passwords do not match<br />";
 	}
 
+	// if there are no errors, save user to database
 	if (count($errors) == 0) {
-			$password = md5($pw); // encrypts pw before storing into database
-			$sql = "INSERT INTO members (fname, lname, password, email) VALUES ('$fname', '$lname', '$password', '$email')";
-			mysql_query($connection, $sql);
-			$_SESSION['fname'] = $fname;
-			$_SESSION['success'] = "You are now logged in";
-			header('location: index.php'); //redirects to home page after success
-		}
+		$password = md5($password); // encrypt password before storing in database (security)
+		$sql = "INSERT INTO `members` (`fname`, `lname`, `password`, `email`)
+				VALUES ('$first_name', '$last_name', '$password', '$email')";
+		mysqli_query($db, $sql);
 
 	}
 
+}
 
 
 
-// logging in from sign in page
-	if(isset($_POST['login'])) {
-		$email = $_POST['email'];
-		$password = $_POST['password'];
-
-		if(empty($email)) {
-			array_push($errors, "Email is required");
-		}
-		if(empty($password)) {
-			array_push($errors, "Password is required");
-		}
-		if (count($errors) == 0) {
-			$password = md5($password); //encrypts password before comparing with the one in database
-			$query = "SELECT * FROM members WHERE email='$email' AND password='$password'";
-			$result = mysqli_query($db, $query);
-
-			if (mysqli_num_rows($result) == 1) {
-				//log user in
-				$_SESSION['email'] = $email;
-				$_SESSION['success'] = "You are now logged in";
-				header('location: index.php'); //redirects to home page after success
-			} else {
-				array_push($errors, "The email/password combination is incorrect");
-			}
-		}
-
-
-
-		mysql_query($connection, $sql);
-	}
-
-
-	//log out
-	if(isset($_GET['logout'])) {
-		session_destroy();
-		unset($_SESSION['email']);
-		header('location: signin.php');
-	}
-
-	?>
+?>
 
 
 
