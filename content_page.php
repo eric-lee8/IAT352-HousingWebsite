52058dcc08ee747162f7bf363984413d58a94aa8
Justin Lau <justin_lau_5@sfu.ca>
2020-11-13 20:02:09 -0800

    renamed housing_db.sql to justin_lau.sql

--- content_page.php
+++ content_page.php
@@ -31,242 +31,245 @@ if(mysqli_connect_errno()) {
 <body>
 
 	<!-- NAVIGATION BAR -->
-    <div class="topnav">
-        <div class="topnav-right">
-            <a href="index.php#home">Home</a>
+	<div class="topnav">
+		<div class="topnav-right">
+			<a href="index.php#home">Home</a>
            <!--  <a href="signup.php">Sign Up</a>
-            <a href="login.php">Log In</a> -->
-            
-            <?php if (!isset($_SESSION['email'])) : ?>
-                <!-- <h3>Welcome <strong><?php echo $_SESSION['email']; ?></strong></h3> -->
-                <div style="display: flex">
-                     <a href="signup.php">Sign Up</a>
-                     <a href="login.php">Log In</a>
-                </div>
+           	<a href="login.php">Log In</a> -->
 
-            <?php endif ?>
+           	<?php if (!isset($_SESSION['email'])) : ?>
+           		<!-- <h3>Welcome <strong><?php echo $_SESSION['email']; ?></strong></h3> -->
+           		<div style="display: flex">
+           			<a href="signup.php">Sign Up</a>
+           			<a href="login.php">Log In</a>
+           		</div>
 
-            <!-- if the user logs in print information about them -->
-            <?php if (isset($_SESSION['email'])) : ?>
-                <!-- <h3>Welcome <strong><?php echo $_SESSION['email']; ?></strong></h3> -->
-                <div style="display: flex">
-                    <p style="color:white">Welcome <strong><?php echo $_SESSION['email']; ?></strong></p>
-                    <a href="profile.php" style="color:white">Edit Profile</a>
-                    <a href="logout.php" style="color:red">Logout</a>
-                </div>
+           	<?php endif ?>
 
-            <?php endif ?>
-        </div>
-    </div>
+           	<!-- if the user logs in print information about them -->
+           	<?php if (isset($_SESSION['email'])) : ?>
+           		<!-- <h3>Welcome <strong><?php echo $_SESSION['email']; ?></strong></h3> -->
+           		<div style="display: flex">
+           			<p style="color:white">Welcome <strong><?php echo $_SESSION['email']; ?></strong></p>
+           			<a href="profile.php" style="color:white">Edit Profile</a>
+           			<a href="logout.php" style="color:red">Logout</a>
+           		</div>
 
-	<!-- IMAGES SECTION -->
-	<div class="images_section center">
-		<div class="main_image">
+           	<?php endif ?>
+           </div>
+       </div>
 
-			
-			<?php
+       <!-- IMAGES SECTION -->
+       <div class="images_section center">
+       	<div class="main_image">
+
+
+       		<?php
 
 			//$LISTING_ID WILL NEED TO TAKE IN THE INPUT FROM THE HOME SCREEN
 
-			$listing_id = $_GET['varname'];
+       		$listing_id = $_GET['varname'];
 			// $listing_id = "R2515566";
 
 			//test
-			$file_path = "Images/";
-			$file_path .= "{$listing_id}/";
-			$file_path .= "{$listing_id}_1.jpg";
+       		$file_path = "Images/";
+       		$file_path .= "{$listing_id}/";
+       		$file_path .= "{$listing_id}_1.jpg";
 
 			// '{$listing_id}'
 
 			// \Images\R2515047
 
-			echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
-			?>
+       		echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
+       		?>
 
-			<!-- HTML CODE -->
-			<!-- <a href= Images/house_1.jpg target="blank"><img src="Images/house_1.jpg" alt="House 1"></a> -->
+       		<!-- HTML CODE -->
+       		<!-- <a href= Images/house_1.jpg target="blank"><img src="Images/house_1.jpg" alt="House 1"></a> -->
 
-		</div>
+       	</div>
 
-		<div class="side_image">
-			
-			<?php
+       	<div class="side_image">
 
-			$file_path = "Images/";
-			$file_path .= "{$listing_id}/";
-			$file_path .= "{$listing_id}_2.jpg";
+       		<?php
 
-			echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
+       		$file_path = "Images/";
+       		$file_path .= "{$listing_id}/";
+       		$file_path .= "{$listing_id}_2.jpg";
 
-			$file_path = "Images/";
-			$file_path .= "{$listing_id}/";
-			$file_path .= "{$listing_id}_3.jpg";
-			echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
+       		echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
 
-			$file_path = "Images/";
-			$file_path .= "{$listing_id}/";
-			$file_path .= "{$listing_id}_4.jpg";
-			echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
-			?>
+       		$file_path = "Images/";
+       		$file_path .= "{$listing_id}/";
+       		$file_path .= "{$listing_id}_3.jpg";
+       		echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
 
-
-			<!-- HTML CODE -->
-			<!-- <a href= Images/house_2.jpg target="blank"><img src="Images/house_2.jpg" alt="House 2"></a> -->
-			<!-- <a href= Images/house_3.jpg target="blank"><img src="Images/house_3.jpg" alt="House 3"></a> -->
-			<!-- <a href= Images/house_4.jpg target="blank"><img src="Images/house_4.jpg" alt="House 4"></a> -->
-		</div>
-	</div>
+       		$file_path = "Images/";
+       		$file_path .= "{$listing_id}/";
+       		$file_path .= "{$listing_id}_4.jpg";
+       		echo "<a href=" . $file_path . " target=\"blank\">" . "<img src=\"".$file_path."\" alt=\"error\"></a>";
+       		?>
 
 
-	<!-- MAIN INFORMATION -->
-	<div class="main_info center">
-		<div class="main_info_left">
+       		<!-- HTML CODE -->
+       		<!-- <a href= Images/house_2.jpg target="blank"><img src="Images/house_2.jpg" alt="House 2"></a> -->
+       		<!-- <a href= Images/house_3.jpg target="blank"><img src="Images/house_3.jpg" alt="House 3"></a> -->
+       		<!-- <a href= Images/house_4.jpg target="blank"><img src="Images/house_4.jpg" alt="House 4"></a> -->
+       	</div>
+       </div>
 
-			<?php 
+
+       <!-- MAIN INFORMATION -->
+       <div class="main_info center">
+       	<div class="main_info_left">
+
+       		<?php 
 					//2. Perform database query
-			$query  = "SELECT * ";
-			$query .= "FROM property ";
+       		$query  = "SELECT * ";
+       		$query .= "FROM property ";
 				//PROPERTY.LISTING_ID WILL NEED TO BE PASSED IN
-			$query .= "WHERE property.listing_id = '{$listing_id}'";
+       		$query .= "WHERE property.listing_id = '{$listing_id}'";
 				// $query .= "WHERE 1";
-			$result = mysqli_query($connection, $query);
+       		$result = mysqli_query($connection, $query);
 					//Test if there was a query error
-			if (!$result) {
-				die("Database query failed.");
-			}
-			?>
+       		if (!$result) {
+       			die("Database query failed.");
+       		}
+       		?>
 
-			<?php
+       		<?php
 				//3. Display all of the orderNumbers as options
-			while ($row = mysqli_fetch_assoc($result)) {
+       		while ($row = mysqli_fetch_assoc($result)) {
 					// grabs the orderNumber row from the database
-				echo "<h2>$" .$row['price']. "</h2>";
-				echo "<h4>" .$row['address'] . " | " . $row['city'] . "</h4>";
-				echo "<h4>" .$row['baths'] . " baths | " . $row['beds'] . " beds</h4>";
-				echo "<h4>" .$row['sqft'] . " SQFT | " .$row['type'] . "</h4>";
-			}
-			?>
-		</div>
-		<div class="main_info_right">
-			<ul>
+       			echo "<h2>$" .$row['price']. "</h2>";
+       			echo "<h4>" .$row['address'] . " | " . $row['city'] . "</h4>";
+       			echo "<h4>" .$row['baths'] . " baths | " . $row['beds'] . " beds</h4>";
+       			echo "<h4>" .$row['sqft'] . " SQFT | " .$row['type'] . "</h4>";
+       		}
+       		?>
+       	</div>
+       	<div class="main_info_right">
+       		<ul>
 
 
-				<!-- ALERT MESSAGE FOR FAVORITING -->
-				<script>
-					function myFunction() {
-						alert("Listing has been added to your favorites!");
-					}
-				</script>
+       			<!-- ALERT MESSAGE FOR FAVORITING -->
+       			<script>
+       				function myFunction() {
+       					alert("Listing has been added to your favorites!");
+       				}
+       			</script>
 
-				<!-- <li><p onclick="alertFunction()">Add to Favorites</p></li> -->
-				<li><button onclick="myFunction()">Add to Favorites</button></li>
-			</ul>
-		</div>
-	</div>
-
-
-	<!-- DETAILS + INSIDE + OUTSIDE -->
-	<div class="details_container center">
-		<div class="details">
-			<h2>DETAILS</h2>
-			<ul>
-				<li>Property Type: Single Family</li>
-				<li>Property Tax: $5,396 (2020)</li>
-				<li>Last Updated: Wed, October 07, 2020</li>
-				<li>Location: Urban</li>
-				<li>Property Subtype: Freehold</li>
-				<li>MLS® #: H4090358</li>
-			</ul>
-		</div>
-
-		<div class="inside">
-			<h2>INSIDE</h2>
-			<ul>
-				<li>Property Type: Single Family</li>
-				<li>Property Tax: $5,396 (2020)</li>
-				<li>Last Updated: Wed, October 07, 2020</li>
-				<li>Location: Urban</li>
-				<li>Property Subtype: Freehold</li>
-				<li>MLS® #: H4090358</li>
-			</ul>
-		</div>
-
-		<div class="outside">
-			<h2>OUTSIDE</h2>
-			<ul>
-				<li>Property Type: Single Family</li>
-				<li>Property Tax: $5,396 (2020)</li>
-				<li>Last Updated: Wed, October 07, 2020</li>
-				<li>Location: Urban</li>
-				<li>Property Subtype: Freehold</li>
-				<li>MLS® #: H4090358</li>
-			</ul>
-		</div>
-	</div>
-
-	<hr class="hr">
-
-	<!-- MORE INFORMATION -->
-	<div class="more_information center">
-		<h2>SINGLE FAMILY IN DUNDAS</h2>
-		<p>A property like this does not come along very often! Very special location, private cul-de-sac and a beautiful setting, this home is situated up high with incredible views of Dundas and the escarpment. Gorgeous setting and treed lot, you will feel like you’re at the cottage or living in a treehouse. Fabulous wraparound deck to enjoy the views, sip your morning coffee or dine outdoors. Home offers charm and character, spacious rooms and abundance of windows. Entertainers kitchen with sitting area or dinette. Large living room and separate den/home office. Don’t overlook the screened in porch! Not only is this a lovely place to enjoy the property but it also serves as perfect private entrance to the den/home office – great space to work from home! Upper level features three spacious bedrooms – notice that the master bedroom features ensuite bathroom. The lower level is currently used as a guest suite and offers a full walk out, separate entrance and patio. This space is fully finished and features completely separate living quarters with kitchen, living room, bedroom, in-suite laundry and full bath. Explore this tranquil property featuring mature gardens, exterior lighting and pathways, hot tub and cedar gazebo. Lots of parking - long interlock driveway leads to attached garage plus separate double driveway. Located close to the Rail Trail, shops, restaurants, schools and McMaster University & Hospital. Don’t be TOO LATE*! *REG TM. RSA
-		</p>
-	</div>
+       			<!-- if the user logs in print information about them -->
+       			<?php if (isset($_SESSION['email'])) : ?>
+       				<!-- <li><p onclick="alertFunction()">Add to Favorites</p></li> -->
+       				<li><button onclick="myFunction()">Add to Favorites</button></li>
+       			<?php endif ?>
+       		</ul>
+       	</div>
+       </div>
 
 
-	<!-- REALTOR INFORMATION -->
-	<div class="realtor center">
-		<img src="Images/realtor_1.jpg" alt="Realtor headshot" width="500" height="600">
+       <!-- DETAILS + INSIDE + OUTSIDE -->
+       <div class="details_container center">
+       	<div class="details">
+       		<h2>DETAILS</h2>
+       		<ul>
+       			<li>Property Type: Single Family</li>
+       			<li>Property Tax: $5,396 (2020)</li>
+       			<li>Last Updated: Wed, October 07, 2020</li>
+       			<li>Location: Urban</li>
+       			<li>Property Subtype: Freehold</li>
+       			<li>MLS® #: H4090358</li>
+       		</ul>
+       	</div>
 
-		<div class="realtor_info">
+       	<div class="inside">
+       		<h2>INSIDE</h2>
+       		<ul>
+       			<li>Property Type: Single Family</li>
+       			<li>Property Tax: $5,396 (2020)</li>
+       			<li>Last Updated: Wed, October 07, 2020</li>
+       			<li>Location: Urban</li>
+       			<li>Property Subtype: Freehold</li>
+       			<li>MLS® #: H4090358</li>
+       		</ul>
+       	</div>
 
-			<?php 
+       	<div class="outside">
+       		<h2>OUTSIDE</h2>
+       		<ul>
+       			<li>Property Type: Single Family</li>
+       			<li>Property Tax: $5,396 (2020)</li>
+       			<li>Last Updated: Wed, October 07, 2020</li>
+       			<li>Location: Urban</li>
+       			<li>Property Subtype: Freehold</li>
+       			<li>MLS® #: H4090358</li>
+       		</ul>
+       	</div>
+       </div>
+
+       <hr class="hr">
+
+       <!-- MORE INFORMATION -->
+       <div class="more_information center">
+       	<h2>SINGLE FAMILY IN DUNDAS</h2>
+       	<p>A property like this does not come along very often! Very special location, private cul-de-sac and a beautiful setting, this home is situated up high with incredible views of Dundas and the escarpment. Gorgeous setting and treed lot, you will feel like you’re at the cottage or living in a treehouse. Fabulous wraparound deck to enjoy the views, sip your morning coffee or dine outdoors. Home offers charm and character, spacious rooms and abundance of windows. Entertainers kitchen with sitting area or dinette. Large living room and separate den/home office. Don’t overlook the screened in porch! Not only is this a lovely place to enjoy the property but it also serves as perfect private entrance to the den/home office – great space to work from home! Upper level features three spacious bedrooms – notice that the master bedroom features ensuite bathroom. The lower level is currently used as a guest suite and offers a full walk out, separate entrance and patio. This space is fully finished and features completely separate living quarters with kitchen, living room, bedroom, in-suite laundry and full bath. Explore this tranquil property featuring mature gardens, exterior lighting and pathways, hot tub and cedar gazebo. Lots of parking - long interlock driveway leads to attached garage plus separate double driveway. Located close to the Rail Trail, shops, restaurants, schools and McMaster University & Hospital. Don’t be TOO LATE*! *REG TM. RSA
+       	</p>
+       </div>
+
+
+       <!-- REALTOR INFORMATION -->
+       <div class="realtor center">
+       	<img src="Images/realtor_1.jpg" alt="Realtor headshot" width="500" height="600">
+
+       	<div class="realtor_info">
+
+       		<?php 
 					//2. Perform database query
-			$query  = "SELECT * ";
-			$query .= "FROM agents ";
-			$query .= "INNER JOIN property ";
+       		$query  = "SELECT * ";
+       		$query .= "FROM agents ";
+       		$query .= "INNER JOIN property ";
 
 				//PROPERTY.LISTING_ID WILL NEED TO BE PASSED IN
-			$query .= "WHERE agents.agent_ID = property.property_agent_ID ";
-			$query .= "AND property.listing_id = '{$listing_id}'";
+       		$query .= "WHERE agents.agent_ID = property.property_agent_ID ";
+       		$query .= "AND property.listing_id = '{$listing_id}'";
 				// $query .= "WHERE 1";
-			$result = mysqli_query($connection, $query);
+       		$result = mysqli_query($connection, $query);
 					//Test if there was a query error
-			if (!$result) {
-				die("Database query failed.");
-			}
-			?>
+       		if (!$result) {
+       			die("Database query failed.");
+       		}
+       		?>
 
-			<?php
+       		<?php
 				//3. Display all of the orderNumbers as options
-			while ($row = mysqli_fetch_assoc($result)) {
+       		while ($row = mysqli_fetch_assoc($result)) {
 					// grabs the orderNumber row from the database
-				echo "<h3>" .$row['name']. "</h3>";
-				echo "<h5>" .$row['ph_no']. "</h5>";
-				echo "<h5>" .$row['email']. "</h5>";
-			}
-			?>
-		</div>
-	</div>
+       			echo "<h3>" .$row['name']. "</h3>";
+       			echo "<h5>" .$row['ph_no']. "</h5>";
+       			echo "<h5>" .$row['email']. "</h5>";
+       		}
+       		?>
+       	</div>
+       </div>
 
 
-	<!-- FOOTER -->
-	<footer class="footer">
-		<p>Team: $teamname</p>
-		<p>Justin Lau, Eric Lee, Lucy Huang</p>
-	</footer>
+       <!-- FOOTER -->
+       <footer class="footer">
+       	<p>Team: $teamname</p>
+       	<p>Justin Lau, Eric Lee, Lucy Huang</p>
+       </footer>
 
-	<script src="JS/content_page.js"></script>
+       <script src="JS/content_page.js"></script>
 
-	<?php
+       <?php
 	// 4. Release returned data
-	mysqli_free_result($result);
-	?>
+       mysqli_free_result($result);
+       ?>
 
-</body>
-</html>
+   </body>
+   </html>
 
-<?php
+   <?php
   // 5. Close database connection
-mysqli_close($connection);
-?>
+   mysqli_close($connection);
+   ?>
